9151458c765c2272811ab7218e54320d670f95e0
Paramvir Singh <paramvir-singh5@myseneca.ca>
2025-08-08 08:41:25 -0400

    Added React Router and minor improvements

--- scriptly/src/App.tsx
+++ scriptly/src/App.tsx
@@ -1,18 +1,22 @@
+import { useRef, useEffect } from "react";
+import HomePage from "./components/Homepage";
+
+import LocomotiveScroll from "locomotive-scroll";
 function App() {
+  const containerRef = useRef(null);
+
+  useEffect(() => {
+    if (containerRef.current) {
+      new LocomotiveScroll({
+        el: containerRef.current,
+        smooth: true,
+      });
+    }
+  }, []);
   return (
-    <>
-      <div>
-        <h1>Scriptly</h1>
-        <p className=" font-extrabold">
-          Welcome to Scriptly, your AI-powered script writing assistant!
-        </p>
-        <p>Start by creating a new script or uploading an existing one.</p>
-        <button onClick={() => alert("Create New Script")}>
-          Create New Script
-        </button>
-        <button onClick={() => alert("Upload Script")}>Upload Script</button>
-      </div>
-    </>
+    <main data-scroll-container ref={containerRef}>
+      <HomePage />
+    </main>
   );
 }
 
--- scriptly/src/main.tsx
+++ scriptly/src/main.tsx
@@ -1,10 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { createRoot } from "react-dom/client";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+import { createBrowserRouter, RouterProvider } from "react-router";
+
+import "./index.css";
+import App from "./App";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    Component: App,
+  },
+]);
+createRoot(document.getElementById("root")!).render(
+  <RouterProvider router={router} />
+);
